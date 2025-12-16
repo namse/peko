6dbe8728f7625bd031cd90011e991bb388846c57
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2025-12-16 10:54:00 +0900

    Remove unused file

--- hq/src/watchdog/host_id.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(
-    Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize,
-)]
-#[repr(transparent)]
-#[serde(transparent)]
-pub struct HostId(pub String);
